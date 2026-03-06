9eff2058b28c7abd3fcc05a3ed95d4bc1d8f6052
Dominic Aquilina <aquilina.dominic@gmail.com>
2026-03-06 18:41:48 -0500

    Cleanup and consistency runthrough

--- src/scripts/types/page.ts
+++ src/scripts/types/page.ts
@@ -1,11 +1,13 @@
 export type Page = {
+  bootstrapFunction?: string;
   canNavigate: boolean;
   children: Array<Page>;
+  description?: string;
   external?: boolean;
   excludeFromNav?: boolean;
   id?: string;
   label: string;
+  notes?: Array<string>;
   path?: string;
-  bootstrapFunction?: string;
   title?: string;
 };
